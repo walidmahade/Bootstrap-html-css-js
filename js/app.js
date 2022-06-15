af1df94785fb03a4de44b3684b2c7b7dd4897148
sakil-ahmed <sakilahmed1495@gmail.com>
2022-06-15 21:15:13 +0600

    responsive

--- js/app.js
+++ js/app.js
@@ -1,24 +1,29 @@
-const dropdownEl = document.querySelectorAll(".navbar .dropdown");
-console.log(dropdownEl);
-// console.log([...dropdownEl]);
+/*
+Custom JavaScript Code
+*/
+const isDesltop = window.matchMedia("(min-width: 991px)");
 
-// add hover event to all dropdown elements
-dropdownEl.forEach((dropdownParent) => {
-    // open dropdown
-    dropdownParent.addEventListener("mouseenter", () => {
-        const dropdownTgl = dropdownParent.querySelector(".dropdown-toggle");
-        // make the dropdown menu visible
-        dropdownTgl.classList.add("show");
-        dropdownTgl.setAttribute("aria-expanded", "true");
-        dropdownTgl.nextElementSibling.classList.add("show");
-    });
+if (isDesltop.matches) {
 
-    // close dropdown
-    dropdownParent.addEventListener("mouseleave", () => {
-        const dropdownTgl = dropdownParent.querySelector(".dropdown-toggle");
-        // make the dropdown menu visible
-        dropdownTgl.classList.remove("show");
-        dropdownTgl.setAttribute("aria-expanded", "false");
-        dropdownTgl.nextElementSibling.classList.remove("show");
+    const dropdownEl = document.querySelectorAll(".navbar .dropdown");
+    // add hover event to all dropdown elements
+
+    dropdownEl.forEach((dropdownParent) => {
+        // open dropdown
+        dropdownParent.addEventListener("mouseenter", () => {
+            const dropdownTgl = dropdownParent.querySelector(".dropdown-toggle");
+            // make the dropdown menu visible
+            dropdownTgl.classList.add("show");
+            dropdownTgl.setAttribute("aria-expanded", "true");
+            dropdownTgl.nextElementSibling.classList.add("show");
+        });
+        // close dropdown
+        dropdownParent.addEventListener("mouseleave", () => {
+            const dropdownTgl = dropdownParent.querySelector(".dropdown-toggle");
+            // make the dropdown menu visible
+            dropdownTgl.classList.remove("show");
+            dropdownTgl.setAttribute("aria-expanded", "false");
+            dropdownTgl.nextElementSibling.classList.remove("show");
+        });
     });
-});
+}
