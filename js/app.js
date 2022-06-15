c9f5aa4e799327819bff4830b0d4da8db8db5c72
nadimshuvo <nadimshuvo1997@gmail.com>
2022-06-15 15:04:04 +0600

    Alhamdulillah

--- js/app.js
+++ js/app.js
@@ -1,24 +1,25 @@
-const dropdownEl = document.querySelectorAll(".navbar .dropdown");
-console.log(dropdownEl);
-// console.log([...dropdownEl]);
+// const dropdownEl = document.querySelectorAll(".navbar .dropdown");
+// console.log(dropdownEl);
+// // console.log([...dropdownEl]);
 
-// add hover event to all dropdown elements
-dropdownEl.forEach((dropdownParent) => {
-  // open dropdown
-  dropdownParent.addEventListener("mouseenter", () => {
-    const dropdownTgl = dropdownParent.querySelector(".dropdown-toggle");
-    // make the dropdown menu visible
-    dropdownTgl.classList.add("show");
-    dropdownTgl.setAttribute("aria-expanded", "true");
-    dropdownTgl.nextElementSibling.classList.add("show");
-  });
+// // add hover event to all dropdown elements
+// dropdownEl.forEach((dropdownParent) => {
+//     // open dropdown
+//     dropdownParent.addEventListener("mouseover", () => {
 
-  // close dropdown
-  dropdownParent.addEventListener("mouseleave", () => {
-    const dropdownTgl = dropdownParent.querySelector(".dropdown-toggle");
-    // make the dropdown menu visible
-    dropdownTgl.classList.remove("show");
-    dropdownTgl.setAttribute("aria-expanded", "false");
-    dropdownTgl.nextElementSibling.classList.remove("show");
-  });
-});
+//         const dropdownTgl = dropdownParent.querySelector(".dropdown-toggle");
+//         // make the dropdown menu visible
+//         dropdownTgl.classList.add("show");
+//         dropdownTgl.setAttribute("aria-expanded", "true");
+//         dropdownTgl.nextElementSibling.classList.add("show");
+//     });
+
+//     // close dropdown
+//     dropdownParent.addEventListener("mouseout", () => {
+//         const dropdownTgl = dropdownParent.querySelector(".dropdown-toggle");
+//         // make the dropdown menu visible
+//         dropdownTgl.classList.remove("show");
+//         dropdownTgl.setAttribute("aria-expanded", "false");
+//         dropdownTgl.nextElementSibling.classList.remove("show");
+//     });
+// });
